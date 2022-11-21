2c86bb98b80ef4d0621c4b7416fc983edb0b482d
Harshil Gajipara <harshilgajipara5@gmail.com>
2022-11-21 19:02:54 +0530

    21/11/2022

--- Data Structure/bubble_sort.c
+++ Data Structure/bubble_sort.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #define n 10
+
 int main()
 {
     int a[n];
-    int i, j, temp;
-
-    for (i = 0; i < n; i++)
-        a[i] = rand() % 99 - 1;
-    printf("Befor Sorted Array : \n");
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
-
-    for (i = 0; i < n; i++)
+    int i,j,temp;
+    for(i=0;i<n;i++)
+        a[i]=rand()%99+1;
+    printf("\n Before Sorting");
+    for(i=0;i<n;i++)
+        printf(" \t %d",a[i]);
+    
+    for(i=0;i<n;i++)
     {
-        for (j = 0; j < n; j++)
+        for(j=0;j<n;j++)
         {
-            if (a[j] > a[j + 1])
+            if(a[j]>a[j+1])
             {
-                temp = a[j];
-                a[j] = a[j + 1];
-                a[j + 1] = temp;
+                temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
             }
         }
     }
-    printf("\n After Sorted Array :\n");
-    for (i = 0; i < n; i++)
-        printf("%d ", a[i]);
+
+    printf("\n After Sorting:");
+    for(i=0;i<n;i++)
+    {
+        printf("\t %d",a[i]);
+    }
 }
