1038d6b43529d77b44b2e04678d1ac9dcd964a52
Harshil Gajipara <harshilgajipara5@gmail.com>
2022-11-24 19:44:51 +0530

    24/11/2022

--- Data Structure/linked_list.c
+++ Data Structure/linked_list.c
@@ -12,7 +12,7 @@ struct node
 
 int main()
 {
-    struct node a,b,c;
+    struct node a,b,c,d,e,f;
     a.n = 65;
     a.p = 'A';
     a.ptr = NULL;
@@ -21,20 +21,46 @@ int main()
     b.ptr = NULL;
     c.n = 67;
     c.p = 'C';
-    c.ptr;
+    c.ptr = NULL;
+    d.n = 68;
+    d.p = 'D';
+    d.ptr = NULL;
+    e.n = 69;
+    e.p = 'E';
+    e.ptr = NULL;
+    f.n = 70;
+    f.p = 'F';
+    f.ptr = NULL;
     
     printf("Var1 : %d\t%c\t",a.n,a.p);
     printf("\nVar2 : %d\t%c",b.n,b.p);
     printf("\nVar3 : %d\t%c",c.n,c.p);
+    printf("\nVar3 : %d\t%c",d.n,d.p);
+    printf("\nVar3 : %d\t%c",e.n,e.p);
+    printf("\nVar3 : %d\t%c",f.n,f.p);
     printf("\n");
     
     a.ptr = &b;
     printf("\nPointer A is link with pointer B");
     printf("\nVar1 : %d\t%c",a.ptr->n,a.ptr->p);
-    printf("\n");
     
     b.ptr = &c;
     printf("\nPointer b is link with pointer c");
     printf("\nVar2 : %d\t%c",b.ptr->n,b.ptr->p);
     
+    c.ptr = &d;
+    printf("\nPointer c is link with pointer d");
+    printf("\nVar3 : %d\t%c",c.ptr->n,c.ptr->p);
+
+    d.ptr = &e;
+    printf("\nPointer d is link with pointer e");
+    printf("\nVar4 : %d\t%c",d.ptr->n,d.ptr->p);
+
+    e.ptr = &f;
+    printf("\nPointer e is link with pointer f");
+    printf("\nVar5 : %d\t%c",e.ptr->n,e.ptr->p);
+
+    f.ptr = &a;
+    printf("\nPointer f is link with pointer a");
+    printf("\nVar6 : %d\t%c",f.ptr->n,f.ptr->p);
 }
